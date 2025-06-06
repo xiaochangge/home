12c532098ef3c81708cb59bc1d5ba3e7146d3d3a
xiaochangge <156281596+xiaochangge@users.noreply.github.com>
2025-06-06 21:36:49 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -67,7 +67,7 @@ export const getWeather = async (key, city) => {
   return await res.json();
 };
 
-// 获取教书先生天气 API
+// 获取教书先生天气 API.
 // https://api.oioweb.cn/doc/weather/GetWeather
 export const getOtherWeather = async () => {
   const res = await fetch("https://api.oioweb.cn/api/weather/GetWeather");
